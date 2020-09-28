e17e04776aefc7a3e65d0d80791e091cf4be51ea
Damian <neo228wp4@gmail.com>
2020-09-28 22:32:39 +0200

    google auth first steps

---
+++ src/backend/note_app/views.py
@@ -0,0 +1,22 @@
+# from django.shortcuts import render
+#
+# from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+#
+#
+# class NoteCreateView(CreateView):
+#     fields = '__all__'
+#     success_url = '/'
+#
+#     pass
+#
+#
+# class NoteDetailView(DetailView):
+#     pass
+#
+#
+# class NoteUpdateView(UpdateView):
+#     pass
+#
+#
+# class NoteDeleteView(DeleteView):
+#     pass
--- src/backend/urls.py
+++ src/backend/urls.py
@@ -1,4 +1,4 @@
-"""src URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="note_app/index.html")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
